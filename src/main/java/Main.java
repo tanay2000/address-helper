5238bd3f4752a76424ae24258dff8dac5e74f67f
Tanay Gupta <tanaygupta@meeshos-MacBook-Pro.local>
2021-04-07 21:11:07 +0530

    count calls from api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,70 +1,31 @@
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import org.supercsv.cellprocessor.ParseDate;
-import org.supercsv.cellprocessor.constraint.NotNull;
-import org.supercsv.cellprocessor.constraint.UniqueHashCode;
-import org.supercsv.cellprocessor.ift.CellProcessor;
-import org.supercsv.io.CsvListReader;
-import org.supercsv.io.ICsvListReader;
-import org.supercsv.prefs.CsvPreference;
-
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-
+        String filename = "All-Messages-search-result.csv";
+        int c=0;
         try {
-            Main.readVariableColumnsWithCsvListReader();
-        } catch (Exception e) {
+            CSVReader csvReader = new CSVReader(new FileReader(filename));
+            String[] nextLine;
+            while((nextLine = csvReader.readNext())!= null){
+                for(String value : nextLine) {
+
+                    if (value.contains("HTTP_CLOUDFRONT_IS_DESKTOP_VIEWER\":[\"true\"]")) {
+                        System.out.println(value);
+                        c++;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(c);
     }
-    private static void readVariableColumnsWithCsvListReader() throws Exception {
+//HTTP_CLOUDFRONT_IS_DESKTOP_VIEWER":["true"]
 
-        final CellProcessor[] allProcessors = new CellProcessor[] { // customerNo (must be unique)
-                new NotNull(),
-                new NotNull(), // firstName
-                new NotNull(), // lastName
-                }; // birthDate
-
-        final CellProcessor[] noBirthDateProcessors = new CellProcessor[] { allProcessors[0], // customerNo
-                allProcessors[1], // firstName
-                allProcessors[2] }; // lastName
-
-        ICsvListReader listReader = null;
-        try {
-            listReader = new CsvListReader(new FileReader("All-Messages-search-result.csv"), CsvPreference.STANDARD_PREFERENCE);
-
-            listReader.getHeader(true); // skip the header (can't be used with CsvListReader)
-
-            while( (listReader.read()) != null ) {
-
-                // use different processors depending on the number of columns
-                final CellProcessor[] processors;
-                if( listReader.length() == noBirthDateProcessors.length ) {
-                    processors = noBirthDateProcessors;
-                } else {
-                    processors = allProcessors;
-                }
-
-                final List<Object> customerList = listReader.executeProcessors(processors);
-                System.out.println(String.format("lineNo=%s, rowNo=%s, columns=%s, customerList=%s",
-                        listReader.getLineNumber(), listReader.getRowNumber(), customerList.size(), customerList));
-            }
-
-        }
-        finally {
-            if( listReader != null ) {
-                listReader.close();
-            }
-        }
-    }
-
-
+    //"\"HTTP_CLOUDFRONT_IS_DESKTOP_VIEWER\":[\"true\"]"
 }
