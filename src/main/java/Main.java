17d5610d778cf17fb0496c11d614578b1a27f433
Tanay Gupta <tanaygupta@meeshos-MacBook-Pro.local>
2021-04-08 00:15:33 +0530

    total calls calculated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
-        String filename = "All-Messages-search-result.csv";
-        int c=0;
+        String filename = "All-Messages-search-result1.csv";
+        int apiCall=0,totalRow=0;
         try {
             CSVReader csvReader = new CSVReader(new FileReader(filename));
             String[] nextLine;
@@ -14,8 +14,10 @@ public class Main {
 
                     if (value.contains("HTTP_CLOUDFRONT_IS_DESKTOP_VIEWER\":[\"true\"]")) {
                         System.out.println(value);
-                        c++;
+                        apiCall++;
                     }
+                    if(value.contains("unknown"))
+                        totalRow++;
                 }
             }
         } catch (FileNotFoundException e) {
@@ -23,9 +25,7 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(c);
+        System.out.println(apiCall);
+        System.out.println(totalRow);
     }
-//HTTP_CLOUDFRONT_IS_DESKTOP_VIEWER":["true"]
-
-    //"\"HTTP_CLOUDFRONT_IS_DESKTOP_VIEWER\":[\"true\"]"
 }
